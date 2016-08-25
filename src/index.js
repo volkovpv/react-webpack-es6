2d6dc0662f12c2c35901c2eaae403562210bb88f
Волков Пётр <p.v.volkov@yandex.ru>
2016-08-26 01:10:49 +0300

    add babel - add config webpack - add src file - add npm module

---
+++ src/index.js
@@ -0,0 +1,16 @@
+/**
+ * Created by https://github.com/volkovpv on 08.2016.
+ */
+
+'use strict';
+
+import React from 'react';
+
+const kkkk = 'lll';
+
+if(NODE_ENV === 'development'){
+    let privatess = true;
+    let privatessss = false;
+    var kkkkss = 'kkkk';
+    console.log(NODE_ENV);
+}
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+/**
+ * Created by https://github.com/volkovpv on 08.2016.
+ */
+
+'use strict';
+
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './src',
+    output: {
+        path: './dist',
+        filename: 'app.js'
+    },
+
+    watch: NODE_ENV === 'development',
+
+    watchOptions: {
+        aggregateTimeout: 100
+    },
+
+    devtool: NODE_ENV === 'development' ? 'source-map' : null,
+
+    plugins: [
+        new webpack.DefinePlugin({
+            NODE_ENV: JSON.stringify(NODE_ENV)
+        })
+    ],
+
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/
+        }]
+    }
+};
